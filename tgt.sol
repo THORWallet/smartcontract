48c4eda368717df11af2eece46a56e8c76d657d5
Thomas Bocek <tom@tomp2p.net>
2021-07-08 15:20:55 +0200

    Update tgt

--- tgt.sol
+++ tgt.sol
@@ -7,42 +7,48 @@ import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+//interface IERC20 comes from openzeppelin
 interface IERC20Metadata is IERC20 {
     function name() external view returns (string memory);
     function symbol() external view returns (string memory);
     function decimals() external view returns (uint8);
 }
 
+// From https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/extensions/draft-IERC20Permit.sol
 interface IERC20Permit {
     function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
     function nonces(address owner) external view returns (uint256);
     function DOMAIN_SEPARATOR() external view returns (bytes32);
 }
 
-interface IERC677 {
-  function transferAndCall(address to, uint value, bytes calldata data) external returns (bool success);
-  event TransferWithData(address indexed from, address indexed to, uint value, bytes data);
+//the function transferFromAndCall was added so that with a permit, also a function can be called
+interface IERC677ish {
+    function transferAndCall(address to, uint256 value, bytes calldata data) external returns (bool success);
+    function transferFromAndCall(address sender, address to, uint256 value, bytes calldata data) external returns (bool success);
+    event TransferWithData(address indexed from, address indexed to, uint256 value, bytes data);
 }
 
 interface IERC677Receiver {
-  function onTokenTransfer(address _sender, uint _value, bytes calldata _data) external;
+  function onTokenTransfer(address sender, uint value, bytes calldata data) external;
 }
 
-contract ERC20 is IERC20Metadata, IERC20Permit, IERC677, EIP712 {
+contract ERC20 is IERC20Metadata, IERC20Permit, IERC677ish, EIP712 {
     mapping(address => uint256) private _balances;
     mapping(address => uint256) private _vesting;
     mapping(address => mapping(address => uint256)) private _allowances;
 
     uint256 private _totalSupply;
-    bool private mintDone = false;
+    uint256 private _live = 0;
     address private _owner;
+    
+    uint256 public constant MAX_SUPPLY = 1000000000 ether;
     
     constructor(string memory _name) EIP712(_name, "1") {
         _owner = msg.sender;
     }
 
     function name() public view virtual override returns (string memory) {
-        return "THORWallet Token";
+        return "THORWallet Governance Token";
     }
 
     function symbol() public view virtual override returns (string memory) {
@@ -90,7 +96,7 @@ contract ERC20 is IERC20Metadata, IERC20Permit, IERC677, EIP712 {
     
     function burn(uint256 amount) public virtual {
         require(_vesting[msg.sender] < block.timestamp);
-        require(mintDone);
+        require(_live != 0);
 
         uint256 accountBalance = _balances[msg.sender];
         require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
@@ -106,7 +112,7 @@ contract ERC20 is IERC20Metadata, IERC20Permit, IERC677, EIP712 {
         require(msg.sender == _owner);
         require(account.length == amount.length);
         require(amount.length == transferableFrom.length);
-        require(!mintDone);
+        require(_live == 0);
         
         for(uint256 i=0;i<account.length;i++) {
             require(account[i] != address(0), "ERC20: mint to the zero address");
@@ -121,9 +127,23 @@ contract ERC20 is IERC20Metadata, IERC20Permit, IERC677, EIP712 {
         }
     }
     
+    function emitTokens() public virtual {
+        uint256 timeInS = block.timestamp - _live;
+        require(timeInS > 0);
+        timeInM = timeInS / (60 * 24 * 30);
+        
+        uint256 supply = MAX_SUPPLY / (2 + timeInM);
+        
+        uint256 missing = supply - _totalSupply;
+        require(missing > 0);
+        _totalSupply += missing;
+        _balances[_owner] += missing;
+        
+    }
+    
     function mintFinish() public virtual {
         require(msg.sender == _owner);
-        mintDone = true;
+        _live = block.timestamp;
     }
 
     function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
@@ -141,32 +161,32 @@ contract ERC20 is IERC20Metadata, IERC20Permit, IERC677, EIP712 {
         return true;
     }
     
-    function transferAndCall(address _to, uint _value, bytes calldata _data) public override returns (bool success) {
-        transfer(_to, _value);
-        emit TransferWithData(msg.sender, _to, _value, _data);
-        if (isContract(_to)) {
-            contractFallback(msg.sender, _to, _value, _data);
+    function transferAndCall(address to, uint value, bytes calldata data) public virtual override returns (bool success) {
+        transfer(to, value);
+        emit TransferWithData(msg.sender, to, value, data);
+        if (isContract(to)) {
+            contractFallback(msg.sender, to, value, data);
         }
         return true;
     }
     
-    function transferFromAndCall(address sender, address _to, uint _value, bytes calldata _data) public returns (bool success) {
-        transferFrom(sender, _to, _value);
-        emit TransferWithData(sender, _to, _value, _data);
-        if (isContract(_to)) {
-            contractFallback(sender, _to, _value, _data);
+    function transferFromAndCall(address sender, address to, uint value, bytes calldata data) public virtual override returns (bool success) {
+        transferFrom(sender, to, value);
+        emit TransferWithData(sender, to, value, data);
+        if (isContract(to)) {
+            contractFallback(sender, to, value, data);
         }
         return true;
     }
 
-    function contractFallback(address sender, address _to, uint _value, bytes calldata _data)  internal virtual {
-        IERC677Receiver receiver = IERC677Receiver(_to);
-        receiver.onTokenTransfer(sender, _value, _data);
+    function contractFallback(address sender, address to, uint value, bytes calldata data) internal virtual {
+        IERC677Receiver receiver = IERC677Receiver(to);
+        receiver.onTokenTransfer(sender, value, data);
     }
 
-    function isContract(address _addr) private view returns (bool hasCode) {
+    function isContract(address addr) private view returns (bool hasCode) {
         uint length;
-        assembly { length := extcodesize(_addr) }
+        assembly { length := extcodesize(addr) }
         return length > 0;
     }
 
@@ -174,7 +194,7 @@ contract ERC20 is IERC20Metadata, IERC20Permit, IERC677, EIP712 {
         require(sender != address(0), "ERC20: transfer from the zero address");
         require(recipient != address(0), "ERC20: transfer to the zero address");
         require(_vesting[sender] < block.timestamp);
-        require(mintDone);
+        require(_live != 0);
 
         uint256 senderBalance = _balances[sender];
         require(senderBalance >= amount, "ERC20: transfer amount exceeds balance");
@@ -189,19 +209,17 @@ contract ERC20 is IERC20Metadata, IERC20Permit, IERC677, EIP712 {
     function _approve(address owner, address spender, uint256 amount) internal virtual {
         require(owner != address(0), "ERC20: approve from the zero address");
         require(spender != address(0), "ERC20: approve to the zero address");
-        require(mintDone);
+        require(_live != 0);
 
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
     
-    //************ ERC777 ********************
-    
+    // ************ ERC777 ********************
     using Counters for Counters.Counter;
     mapping (address => Counters.Counter) private _nonces;
     bytes32 private immutable _PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     
-
     function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) public virtual override {
         require(block.timestamp <= deadline, "ERC20Permit: expired deadline");
         bytes32 structHash = keccak256(abi.encode(_PERMIT_TYPEHASH, owner, spender, value, _useNonce(owner), deadline));
