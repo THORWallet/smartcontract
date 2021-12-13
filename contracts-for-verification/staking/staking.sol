83afed22d069748c2735e650a06034149ca9945e
severinwullschleger <severin.wullschleger@bluewin.ch>
2021-12-13 16:16:39 +0100

    renaming

--- contracts-for-verification/staking/staking.sol
+++ contracts-for-verification/staking/staking.sol
@@ -8,11 +8,11 @@
 
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Multicall.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./IERC20.sol";
+import "./SafeERC20.sol";
+import "./Ownable.sol";
+import "./Multicall.sol";
+import "./ReentrancyGuard.sol";
 
 contract Staking is Ownable, Multicall, ReentrancyGuard {
     using SafeERC20 for IERC20;
